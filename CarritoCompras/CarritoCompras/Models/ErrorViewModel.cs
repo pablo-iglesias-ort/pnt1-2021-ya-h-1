12a9f4fce9c4d96bdb3f5e1a5f0b09afbd18be97
Marcelo <marcelogomezgrois@gmail.com>
2021-08-24 23:10:53 -0300

    primer commit

---
+++ CarritoCompras/CarritoCompras/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CarritoCompras.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
