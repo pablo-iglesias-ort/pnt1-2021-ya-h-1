5e4c4b2ad03104907b627c7deab3a5279ba7915b
Marcelo <marcelogomezgrois@gmail.com>
2021-09-07 20:29:22 -0300

    nada

--- CarritoCompras/CarritoCompras/Data/CarritoComprasContext.cs
+++ CarritoCompras/CarritoCompras/Data/CarritoComprasContext.cs
@@ -15,5 +15,7 @@ namespace CarritoCompras.Data
         }
 
         public DbSet<CarritoCompras.Models.Producto> Producto { get; set; }
+
+        public DbSet<CarritoCompras.Models.Carrito> Carrito { get; set; }
     }
 }
