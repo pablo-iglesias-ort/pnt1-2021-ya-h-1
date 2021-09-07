200b1ebf6a5f4e321c53b7d2d5fc0321e6f04c1a
jhpierani <j.pierani@gmail.com>
2021-09-07 16:00:17 -0300

    agrego las clases Sucursal y StockItem

---
+++ CarritoCompras/CarritoCompras/Models/StockItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarritoCompras.Models
+{
+    public class StockItem
+    {
+        public Guid Id { get; set; }
+        public Sucursal sucursal { get; set; }
+        public Producto producto { get; set; }
+        public int cantidad { get; set; }
+
+    }
+}
---
+++ CarritoCompras/CarritoCompras/Models/Sucursal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarritoCompras.Models
+{
+    public class Sucursal
+    {
+        public Guid Id { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+        public List<StockItem> stockItems { get; set; }
+
+    }
+}
