ccc230beeff262e01be569233339e9f68d984bec
Marcelo <marcelogomezgrois@gmail.com>
2021-09-08 23:57:29 -0300

    Carrito y CarritoItem Completos

--- CarritoCompras/CarritoCompras/Data/CarritoComprasContext.cs
+++ CarritoCompras/CarritoCompras/Data/CarritoComprasContext.cs
@@ -16,15 +16,13 @@ namespace CarritoCompras.Data
 
         public DbSet<CarritoCompras.Models.Producto> Producto { get; set; }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> f953aa4d2b4ce7d01151ed4f5a2fd2ffff8d01da
         public DbSet<CarritoCompras.Models.Carrito> Carrito { get; set; }
 
         public DbSet<CarritoCompras.Models.Sucursal> Sucursal { get; set; }
 
         public DbSet<CarritoCompras.Models.StockItem> StockItem { get; set; }
+
+        public DbSet<CarritoCompras.Models.CarritoItem> CarritoItem { get; set; }
 
     }
 }
