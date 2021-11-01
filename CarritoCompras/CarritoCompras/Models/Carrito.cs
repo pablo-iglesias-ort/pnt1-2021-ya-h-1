dda6a65e5eaee486cc4a85df640005e48c47597d
Marcelo <marcelogomezgrois@gmail.com>
2021-10-31 23:14:48 -0300

    Solucionado ForeignKey Carrito/Cliente

--- CarritoCompras/CarritoCompras/Models/Carrito.cs
+++ CarritoCompras/CarritoCompras/Models/Carrito.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,13 +11,17 @@ namespace CarritoCompras.Models
     {
         private const string completarObligatorio = "Falta completar {0} , Obligatorio";
         [Required(ErrorMessage = completarObligatorio)]
+
         public Guid CarritoId { get; set; }
         [Required(ErrorMessage = completarObligatorio)]
+
+        [ForeignKey(nameof(Cliente))]
         public Guid ClienteId { get; set; }
-        [Required(ErrorMessage = completarObligatorio)]
-        public bool Activo { get; set; }
-        [Required(ErrorMessage = completarObligatorio)]
         public Cliente Cliente { get; set; }
+
+        [Required(ErrorMessage = completarObligatorio)]
+        public bool Activo { get; set; }               
+
         public List<CarritoItem> CarritosItems { get; set; }
 
         [Required(ErrorMessage = completarObligatorio)]
